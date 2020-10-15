f0df86b05d5c8c77e42b01dd7bb43fd22dac1c37
Egor Khrapak <bkkshaman@gmail.com>
2020-10-16 02:38:52 +0700

    Initial commit

---
+++ src/components/AddUser.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react'; 
+import { GlobalContext } from '../context/GlobalState';
+import { Link, useHistory } from 'react-router-dom';
+import {
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Button
+} from 'reactstrap';
+
+export const AddUser = () => {
+    const { addUser } = useContext(GlobalContext);
+    const history = useHistory();
+
+    const onSubmit = () => {
+        const newUser = {
+            id: 4,
+            name: 'User Four'
+        }
+        addUser(newUser);
+        history.push('/');
+
+    }
+    
+    return (
+    <Form onSubmit={onSubmit}>
+        <FormGroup>
+            <Label>Name</Label>
+            <Input type="text" placeholder="Enter Name" ></Input>
+        </FormGroup>
+          <Button type="submit">Submit</Button>
+          <Link to="/" className="btn btn-danger ml-2">Cancel</Link>
+    </Form>
+    )
+}
---
+++ src/context/GlobalState.js
@@ -0,0 +1,44 @@
+import React, {createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+// Initial State
+const initialState = {
+    users: [
+        {id: 1, name: 'User One'},
+        {id: 2, name: 'User Two'},
+        {id: 3, name: 'User Three'}
+    ]
+};
+
+//Create Context
+export const GlobalContext = createContext(initialState);
+
+//Provider COmponent
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer (AppReducer, initialState);
+    
+// Actions
+const removeUser = (id) => {
+    dispatch({
+        type: 'REMOVE_USER',
+        payload: id
+    })
+}
+
+const addUser = (user) => {
+    dispatch({
+        type: 'ADD_USER',
+        payload: user
+    })
+}
+
+    return (
+        <GlobalContext.Provider value={{
+            users: state.users,
+            removeUser,
+            addUser
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
